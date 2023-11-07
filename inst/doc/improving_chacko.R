8f5dc83ef1e2684e9e9de14ef78f7819b939a498
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-11-07 08:40:02 +0000

    version 0.2.0

---
+++ inst/doc/improving_chacko.R
@@ -0,0 +1,12 @@
+## ----reduction----------------------------------------------------------------
+library(permChacko)
+reduceVector(chacko66_sec3)
+reduceVector(chacko66_sec5)
+
+## ----table--------------------------------------------------------------------
+chacko63_tab1
+
+## ----p-values-----------------------------------------------------------------
+permChacko(chacko66_sec3)
+permChacko(chacko66_sec5)
+
